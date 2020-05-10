370fa633ceac6eac93508107ec7a3b431142c1aa
Carlos <dex076@hotmail.com>
2020-05-09 22:17:35 -0700

    Level 3 complete

---
+++ Go Danny, Go!/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("GAME OVER");
+            //Restarts game
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+    void Restart()
+    {
+        //Loads Current scene 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
